37c2a285c2b26b82a85c6cabb4dd1d4c7a3831ee
Joshua Bray <josh.c.bray@gmail.com>
2022-06-01 14:42:04 +0700

    Refactor book-card and add del btn

--- js-advanced-1/source.js
+++ js-advanced-1/source.js
@@ -15,20 +15,24 @@ function Book(title, author, pages, read) {
   }
 }
 
-const booka = new Book("a", "b", 4, "read");
-const bookb = new Book("b", "c", 87, "read");
+// TEMPORARY INITIALISED BOOKS FOR TESTING
+// const booka = new Book("a", "b", 4, "read");
+// const bookb = new Book("b", "c", 87, "read");
+// myLibrary.push(booka);
+// myLibrary.push(bookb);
+// console.log(myLibrary);
+// function addBookToLibrary() {
+//   for(let i = 0; i < myLibrary.length; i++) {
+//     // for each book in library, collate info
+//     let book = myLibrary[i];
+//     addABookToLibrary(book);
+//   }
+// }
+// addBookToLibrary();
 
-myLibrary.push(booka);
-myLibrary.push(bookb);
-console.log(myLibrary);
 
-
-function addBookToLibrary() {
-  for(let i = 0; i < myLibrary.length; i++) {
-
-    // for each book in library, collate info
-    let book = myLibrary[i];
-    let txt = "";
+function addABookToLibrary(book) {
+  let txt = "";
     for (let property in book) {
       if (property != "info") {
         txt += book[property] + " ";
@@ -38,9 +42,77 @@ function addBookToLibrary() {
     // create a card for each book
     let card = document.createElement("div");
     card.className = "card";
-    card.innerHTML = txt;
+    card.dataset.index = myLibrary.length -1;
+
+    let para = document.createElement("p");
+    para.innerHTML = txt;
+    
+    let delBtn = document.createElement("button");
+    delBtn.id = "delBtn";
+
+    card.append(para);
+    card.append(delBtn);
     document.querySelector(".container").append(card);
+
+    
+    console.log(myLibrary.length);
+}
+
+
+
+const newbookbtn = document.getElementById("newBook");
+newbookbtn.addEventListener("click", addNewBook);
+
+function addNewBook() {
+  const form = document.getElementById('form');
+  if (form.style.display === 'none') {
+    form.style.display = 'block';
+  } else {
+    form.style.display = 'none';
   }
 }
 
-addBookToLibrary();
+
+const submitBtn = document.getElementById("submit");
+submitBtn.addEventListener("click", submitNewBook);
+
+function submitNewBook(event) {
+  event.preventDefault(); // stops btn click submission
+  let title = document.getElementById("title").value
+  let author = document.getElementById("author").value
+  let numberOfPages = document.getElementById("number-of-pages").value
+  let read = document.getElementById("read").value
+  let book = new Book(title, author, numberOfPages, read)
+  myLibrary.push(book);
+  document.forms[0].reset();
+  console.warn('added' , {myLibrary} );
+  addABookToLibrary(book);
+}
+
+
+//   let movies = [];
+//         // example {id:1592304983049, title: 'Deadpool', year: 2015}
+//         const addMovie = (ev)=>{
+//             ev.preventDefault();  //to stop the form submitting
+//             let movie = {
+//                 id: Date.now(),
+//                 title: document.getElementById('title').value,
+//                 year: document.getElementById('yr').value
+//             }
+//             movies.push(movie);
+//             document.forms[0].reset(); // to clear the form for the next entries
+//             //document.querySelector('form').reset();
+
+//             //for display purposes only
+//             console.warn('added' , {movies} );
+//             let pre = document.querySelector('#msg pre');
+//             pre.textContent = '\n' + JSON.stringify(movies, '\t', 2);
+
+//             //saving to localStorage
+//             localStorage.setItem('MyMovieList', JSON.stringify(movies) );
+//         }
+//         document.addEventListener('DOMContentLoaded', ()=>{
+//             document.getElementById('btn').addEventListener('click', addMovie);
+//         });
+
+// }
