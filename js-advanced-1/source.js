7bf584de7ec5f2af4ad39e9dd9a9f6ffa5d84e98
Joshua Bray <josh.c.bray@gmail.com>
2022-06-01 17:28:18 +0700

    Fix read Boolean in preparation for toggle boolean

--- js-advanced-1/source.js
+++ js-advanced-1/source.js
@@ -8,10 +8,14 @@ function Book(title, author, pages, read) {
   this.read = read;
   this.info = function() {
     let infoString;
+    console.log(read);
     if(read) {
-      infoString = `${title} by ${author}, ${pages} pages, not read yet`;
-    } infoString = `${title} by ${author}, ${pages} pages, read`;
-    console.log(infoString);
+      console.log("yay");
+      infoString = `${title} by ${author}, ${pages} pages, read!`;
+    } else {
+        infoString = `${title} by ${author}, ${pages} pages, not read yet.`;
+      }
+    return infoString;
   }
 }
 
@@ -32,12 +36,12 @@ function Book(title, author, pages, read) {
 
 
 function addABookToLibrary(book) {
-  let txt = "";
-    for (let property in book) {
-      if (property != "info") {
-        txt += book[property] + " ";
-      }
-    };
+  // let txt = "";
+  //   for (let property in book) {
+  //     if (property != "info") {
+  //       txt += book[property] + " ";
+  //     }
+  //   };
     
     // create a card for each book
     let card = document.createElement("div");
@@ -45,16 +49,25 @@ function addABookToLibrary(book) {
     card.dataset.index = myLibrary.length -1;
 
     let para = document.createElement("p");
-    para.innerHTML = txt;
+    para.innerHTML = book.info();
 
     let delBtn = document.createElement("button");
     delBtn.innerHTML = "DELETE";
     delBtn.id = "delBtn";
     delBtn.addEventListener("click", removeCard);
 
+    let readBtn = document.createElement("button");
+    readBtn.innerHTML = "Toggle read";
+    readBtn.id = "readBtn";
+    readBtn.addEventListener("click", toggleRead);
+
     card.append(para);
     card.append(delBtn);
     document.querySelector(".container").append(card);
+}
+
+function toggleRead(event) {
+  
 }
 
 function removeCard(event) {
@@ -97,39 +110,13 @@ function submitNewBook(event) {
   let title = document.getElementById("title").value
   let author = document.getElementById("author").value
   let numberOfPages = document.getElementById("number-of-pages").value
-  let read = document.getElementById("read").value
+  console.log(document.getElementById("read").checked)
+  let read = document.getElementById("read").checked;
+  console.log(`2` + read);
   // combine into object
   let book = new Book(title, author, numberOfPages, read)
   myLibrary.push(book);
+  console.log(book);
   document.forms[0].reset();
-  console.warn('added' , {myLibrary} );
   addABookToLibrary(book);
 }
-
-
-//   let movies = [];
-//         // example {id:1592304983049, title: 'Deadpool', year: 2015}
-//         const addMovie = (ev)=>{
-//             ev.preventDefault();  //to stop the form submitting
-//             let movie = {
-//                 id: Date.now(),
-//                 title: document.getElementById('title').value,
-//                 year: document.getElementById('yr').value
-//             }
-//             movies.push(movie);
-//             document.forms[0].reset(); // to clear the form for the next entries
-//             //document.querySelector('form').reset();
-
-//             //for display purposes only
-//             console.warn('added' , {movies} );
-//             let pre = document.querySelector('#msg pre');
-//             pre.textContent = '\n' + JSON.stringify(movies, '\t', 2);
-
-//             //saving to localStorage
-//             localStorage.setItem('MyMovieList', JSON.stringify(movies) );
-//         }
-//         document.addEventListener('DOMContentLoaded', ()=>{
-//             document.getElementById('btn').addEventListener('click', addMovie);
-//         });
-
-// }
