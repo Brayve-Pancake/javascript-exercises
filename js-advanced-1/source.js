d10f9cb11a294e6a081e5da60ccd72025b81c057
Joshua Bray <josh.c.bray@gmail.com>
2022-06-01 18:09:03 +0700

    Refine code and add comments for posterity

--- js-advanced-1/source.js
+++ js-advanced-1/source.js
@@ -1,71 +1,70 @@
-let myLibrary = [];
+// Start the library
+const myLibrary = [];
 
-
+// Create constructor for all new books
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
   this.info = function() {
-    let infoString;
-    console.log(read);
-    if(read) {
-      console.log("yay");
-      infoString = `${title} by ${author}, ${pages} pages, read!`;
-    } else {
-        infoString = `${title} by ${author}, ${pages} pages, not read yet.`;
-      }
+    // Create a msg to display
+    let infoString = `${title} by ${author}, ${pages} pages, `;
+    // Append based on "read" bool;
+    infoString += this.read? "read!": "not read yet.";
     return infoString;
   }
 }
 
-function addABookToLibrary(book) {
+function addBookToLibrary(book) {
     // create a card for each book
     let card = document.createElement("div");
     card.className = "card";
     card.dataset.index = myLibrary.length -1;
 
     let para = document.createElement("p");
+    para.id = "pInfo";
     para.innerHTML = book.info();
 
     let delBtn = document.createElement("button");
     delBtn.innerHTML = "DELETE";
-    delBtn.id = "delBtn";
     delBtn.addEventListener("click", removeCard);
 
     let readBtn = document.createElement("button");
     readBtn.innerHTML = "Toggle read";
-    readBtn.id = "readBtn";
     readBtn.addEventListener("click", toggleRead);
 
-    card.append(para);
-    card.append(delBtn);
-    card.append(readBtn);
+    card.append(para, delBtn, readBtn);
+
+    // Append the card to the grid container
     document.querySelector(".container").append(card);
 }
 
 function toggleRead(event) {
+  // Get dataset value of event listener
   let index = event.target.parentElement.dataset.index;
+  // Change the "read" bool 
   let book = myLibrary[index];
-  console.log(book.read);
   book.read = !book.read;
-  console.log(book.read);
+  // Identify the relevant card and refresh it's HTML
+  let para = document.getElementById("pInfo");
+  para.innerHTML = book.info();
 }
 
 function removeCard(event) {
-  // get the card's data-index value
+  // Get dataset value of event listener
   let index = event.target.parentElement.dataset.index;
-  // remove the card from display
   let container = document.querySelector(`[data-index="${index}"]`);
   container.remove();
-  // remove book from library using dataset value, which is set to it's index
+  // Remove/ Overwrite book to null
   myLibrary[index] = {};    
 };
 
 
-
-const newbookbtn = document.getElementById("newBook");
-newbookbtn.addEventListener("click", () => {
+// ADD A NEW BOOK
+const newBookBtn = document.getElementById("newBook");
+newBookBtn.addEventListener("click", () => {
+  // Display/hide "new book" form 
   const form = document.getElementById('form');
   if (form.style.display === 'none') {
     form.style.display = 'block';
@@ -74,31 +73,20 @@ newbookbtn.addEventListener("click", () => {
   }
 });
 
-// const delBtn = document.getElementById("delBtn");
-// delBtn.addEventListener("click", () => {
-//   // remove the card from display
-//   // document.querySelector("[data-index='0']").style.display = none;
-  
-//   // remove book from library using dataset value, which is set to it's index
-// });
-
-
 const submitBtn = document.getElementById("submit");
 submitBtn.addEventListener("click", submitNewBook);
 
 function submitNewBook(event) {
   event.preventDefault(); // stops btn click submission
-  // get relevant info
+  // Get info from form
   let title = document.getElementById("title").value
   let author = document.getElementById("author").value
   let numberOfPages = document.getElementById("number-of-pages").value
-  console.log(document.getElementById("read").checked)
   let read = document.getElementById("read").checked;
-  console.log(`2` + read);
-  // combine into object
+  // Use into create new book
   let book = new Book(title, author, numberOfPages, read)
   myLibrary.push(book);
-  console.log(book);
+  // Reset the form after submission
   document.forms[0].reset();
-  addABookToLibrary(book);
+  addBookToLibrary(book);
 }
