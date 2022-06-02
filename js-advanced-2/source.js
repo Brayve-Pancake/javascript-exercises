9ee3b48b578258c860d8da5d9fe9f1e4c9edd9f8
Joshua Bray <josh.c.bray@gmail.com>
2022-06-02 15:47:44 +0700

    Add 'place tic-tac' function

--- js-advanced-2/source.js
+++ js-advanced-2/source.js
@@ -1,14 +1,26 @@
-const Gameboard = (() => {
-  const gameboard = [];
-})();
+// const Gameboard = (() => {
+//   const gameboard = [];
+// })();
 
 
 
-const Players = (name, playerNumber) => {
-  const name = name;
-  const playerNumber = playerNumber;
-}
+// const Players = (name, playerNumber) => {
+//   const name = name;
+//   const playerNumber = playerNumber;
+// }
 
-const NewGame = (player) => {
-  
+// const NewGame = (player) => {
+
+// }
+
+const card = document.querySelectorAll(".card");
+card.forEach((x) => {x.addEventListener("click", placeCounter)});
+
+function placeCounter() {
+  console.log(this);
+  let cardClicked = this;
+  const ticTac = document.createElement("p");
+  ticTac.innerHTML = "O";
+  this.append(ticTac);
+  this.removeEventListener("click", placeCounter);
 }
