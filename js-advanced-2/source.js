1e358d4dec3501a31e2d86173784e412643c061b
Joshua Bray <josh.c.bray@gmail.com>
2022-06-02 16:39:12 +0700

    Move placeCounter to Gameboard

--- js-advanced-2/source.js
+++ js-advanced-2/source.js
@@ -1,26 +1,31 @@
-// const Gameboard = (() => {
-//   const gameboard = [];
-// })();
+const Gameboard = (() => {
+  const gameboard = [];
+
+  // Allow each cell to be clicked once
+  const card = document.querySelectorAll(".card");
+  card.forEach((x) => {x.addEventListener("click", placeCounter)});
+  // Cannot be arrow because I need to removeEventListener
+  function placeCounter() {
+    const ticTac = document.createElement("p");
+    ticTac.innerHTML = "O";
+    this.append(ticTac);
+    this.removeEventListener("click", placeCounter);
+  }
+
+})();
 
 
 
-// const Players = (name, playerNumber) => {
-//   const name = name;
-//   const playerNumber = playerNumber;
-// }
+const Players = () => {
+  // when player clicks cell, record the play in gameboard
+  //
+
+}
 
 // const NewGame = (player) => {
 
 // }
 
-const card = document.querySelectorAll(".card");
-card.forEach((x) => {x.addEventListener("click", placeCounter)});
-
-function placeCounter() {
-  console.log(this);
-  let cardClicked = this;
-  const ticTac = document.createElement("p");
-  ticTac.innerHTML = "O";
-  this.append(ticTac);
-  this.removeEventListener("click", placeCounter);
-}
+// in  a game, each player takes a turn to select a cell
+// if it is playerOne's turn, then change the Tic to Tac
+// each player can placeCounter
